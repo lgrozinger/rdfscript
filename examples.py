38a30e0ff8441da5fabf57da483e7b2dc23a314c
Matthew <mattycrowther1@hotmail.co.uk>
2019-10-07 17:13:11 +0100

    Updated

--- examples.py
+++
@@ -1,9 +0,0 @@
-import run
-
-
-def examples():
-    # python run.py -s rdfxml examples/templates.rdfsh -o <output-file>
-    run.parse_from_file("examples/templates.rdfsh","rdfxml",[],"test.xml",[],1 )
-
-
-if __name__ == "__main__": examples()
